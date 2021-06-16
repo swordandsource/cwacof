2706d18be7733f64c2c6e3126ed3db6ffd4dceec
Adam Waselnuk <adam.waselnuk@gmail.com>
2021-06-16 11:29:04 -0400

    Setup basic structure and add some notes for ideas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Helooooo sir");
+import { sample } from "lodash";
+import etherealEffects from "../data/maze-rats-ethereal-effects.json";
+import etherealElements from "../data/maze-rats-ethereal-elements.json";
+import etherealForms from "../data/maze-rats-ethereal-forms.json";
+import physicalEffects from "../data/maze-rats-physical-effects.json";
+import physicalElements from "../data/maze-rats-physical-elements.json";
+import physicalForms from "../data/maze-rats-physical-forms.json";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const spell = rollSpell();
+  renderSpell(spell);
+});
+
+function renderSpell(spell) {
+  const spellEffectNode = document.getElementById("spell-effect");
+  spellEffectNode.innerText = spell.effect;
+}
+
+function rollSpell() {
+  return {
+    effect: rollSpellEffect(),
+  };
+}
+
+function rollSpellEffect() {
+  return `${sample(physicalEffects)} ${sample(etherealForms)}`;
+}
